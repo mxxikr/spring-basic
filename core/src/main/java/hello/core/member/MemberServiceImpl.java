a9448a425b545a1f999689bedb284dd4541ec658
mxxikr <mxxikr@gmail.com>
2026-01-11 10:41:09 +0900

    컴포넌트 스캔: 컴포넌트 스캔과 의존 관계 자동 주입 시작하기

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    // 생성자를 통해 의존성 주입을 받음
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository; // memberRepository는 생성자 통해서 구현체가 주입됨
     }
