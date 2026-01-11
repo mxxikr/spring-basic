f1da47c1f016311cb716de619fd8c78eac26d0f3
mxxikr <mxxikr@gmail.com>
2026-01-11 11:11:43 +0900

    컴포넌트 스캔: 중복과 충돌

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class),
         basePackages = "hello.core")
 public class AutoAppConfig {
+//    @Bean(name = "memoryMemberRepository") // 수동 빈 등록이 우선권을 가짐
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
