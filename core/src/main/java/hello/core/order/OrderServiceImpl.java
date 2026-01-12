8ad4d42468b773ca4e905bad12c6a0453123074a
mxxikr <mxxikr@gmail.com>
2026-01-13 04:55:22 +0900

    의존 관계 자동 주입: @Autowired 필드 명, @Qualifier, @Primary - @Autowired 필드 명 매칭

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,15 +4,23 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    //테스트 용도
+    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // 할인 정책을 인터페이스로 변경하여 유연성을 높임
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -20,10 +28,5 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책을 사용하여 할인 금액 계산
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-
-    //테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
     }
 }
